64678024129a7a58553fb6ecd81929988bd794e9
jamesmc2 <jamesmc21998@gmail.com>
2020-02-24 16:22:14 -0600

    login app added

---
+++ login_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('register', views.register),
+    path('login', views.login),
+    path('success', views.success),
+    path('logout', views.logout),
+    path('login', views.login),
+    path('delete', views.delete)
+]
