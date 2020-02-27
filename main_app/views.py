a81fb17952ee641b3e1d201d01aeadd4c2f4282c
wknobbe <wknobbe4@gmail.com>
2020-02-27 10:14:52 -0600

    page templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def social(request):
+    return render(request, 'social.html')
